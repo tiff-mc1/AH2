d0fe162cc93265cefa7639de14a8e299cec01d7b
McNerlin <tiffany.mcnerlin@fiserv.com>
2023-10-10 15:47:53 -0400

    updated isPrime solution

--- Solution.java
+++ Solution.java
@@ -1,22 +1,26 @@
 import java.util.Scanner;
 
 class Solution {
-    public static void main (String[] args) {
-        Scanner console = new Scanner(System.in);
-        System.out.print("Please enter a number under 100: ");
-        int num = console.nextInt();
-
-        public static boolean isPrime(int num) {
+    public static boolean isPrime(int num) {
             if (num <= 1) {
-                return True;
+                return true;
             }
             
             for (int i = 2; i <= num/2; i++) {
                 if (num % i == 0) {
-                    return False;
+                    return false;
                 }
             }
-            return True;
+            return true;
         }
-    }
+    public static void main (String[] args) {
+        Scanner console = new Scanner(System.in);
+        try {
+            System.out.print("Please enter a number under 100: ");
+            int num = console.nextInt();
+            isPrime(num);
+        } finally {
+            console.close();
+        }   
+    }   
 }
